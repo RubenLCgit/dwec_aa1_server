6c8d4a87863c7370a3015424a9df8b76fa3060b7
RubenLCgit <125395496+RubenLCgit@users.noreply.github.com>
2023-12-20 12:51:47 +0100

    add: Javascript code to add categories

--- www/src/index.js
+++ www/src/index.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   fetch("http://localhost:3000/categories")
     .then((res) => res.json())
-    .then((data) => drawData(data));
+    .then((data) => showCategories(data));
 
-  let drawData = (data) => {
+  //SHOW CATEGORIES
+
+  let showCategories = (data) => {
     data.forEach((category) => {
       let parent = document.getElementsByTagName("ul")[0];
       let child = document.createElement("li");
@@ -14,5 +16,39 @@ document.addEventListener("DOMContentLoaded", function () {
       parent.appendChild(child);
       child2.textContent = category.name;
     });
+  };
+
+  //ADD CATEGORY
+
+  const buttonAddCategory = document.getElementById("button-category-add");
+  buttonAddCategory.addEventListener("click", function () {
+    let nameCategory;
+    do {
+      nameCategory = prompt("Enter a category name", "");
+      if (nameCategory === null) {
+        alert("Operation canceled by user.");
+        break;
+      }
+      if (nameCategory.trim() === "") {
+        alert("Please enter a valid name.");
+      }
+    } while (nameCategory === null || nameCategory.trim() === "");
+    addCategory(nameCategory);
+  });
+
+  let addCategory = (name) => {
+    const nameCategory = {
+      name: name,
+    };
+    fetch("http://localhost:3000/categories", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(nameCategory),
+    })
+      .then((res) => res.json())
+      .then((data) => showCategories(data));
+    location.reload();
   };
 });
